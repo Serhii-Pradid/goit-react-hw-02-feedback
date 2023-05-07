3d23e36c865b5232312169ef817effe63d009f4c
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-05-07 22:12:01 +0300

    styled 1

--- src/components/FeedbackOptions/feedbackOptions.jsx
+++ src/components/FeedbackOptions/feedbackOptions.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
+import css from 'components/FeedbackOptions/FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     
     return (
     
-        <div>
+        <div className={css.feedback}>
             {options.map(btn => (
-                <button
+                <button 
                 key={btn}
                 type='button'
                 name={btn}
-                onClick={() => onLeaveFeedback(btn)}>
-                {btn}
+                onClick={() => onLeaveFeedback(btn)} className={css.btnOnClick} >
+                <span className={css.btnTitle}>{btn}</span>
             </button>
              ))}
         </div>
